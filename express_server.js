cc836dcdfb55c298459020560821473bf8f3a480
Varsha Panchal <varsha3051@gmail.com>
2022-01-26 01:20:54 -0500

    create login logout

--- express_server.js
+++ express_server.js
@@ -25,20 +25,28 @@ app.set("view engine", "ejs");
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
 // });
 
+// middleware
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser');
 
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+//All URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: req.cookies["username"] ? urlDatabase : {}};
+
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -68,6 +76,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username)
+  res.redirect('/urls')
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect('/urls')
+  
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
