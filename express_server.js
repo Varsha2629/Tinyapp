1f4ce484f9e9fd3042c1e9c98514511332036a1c
Varsha Panchal <varsha3051@gmail.com>
2022-01-27 14:08:58 -0500

    restrictated edit and delete

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 
+
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -13,8 +14,15 @@ const generateRandomString = () => {
 }
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  ur4qtm: {
+    longURL: "www.Meta.com",
+    user_id: "userRandomID",
+  },
+  ozsgzu: {
+    longURL: "www.google.com",
+    user_id: "userRandomID",
+  }
+  
 };
 
 const users = {
@@ -39,52 +47,116 @@ const emailCheck = (db, email) => {
   return false;
 }
 
+const getUserUrls = (db, cookieUser) => {
+  // console.log(db, cookieUser)
+  let userUrls = {};
+
+  for (let key of Object.keys(db)) {
+    if (db[key]["user_id"] === cookieUser) {
+      userUrls[key] = {
+        longURL: urlDatabase[key]["longURL"],
+        user_id: urlDatabase[key]["user_id"],
+      };
+    }
+    
+  }
+  console.log('heiw', userUrls)
+  return userUrls;
+}
 app.set("view engine", "ejs");
 
-
-
 //All URLs(routes)
+// app.get("/", (req, res) => {
+//   if (!users[req.cookies["user_id"]]) {
+//     return res.redirect("/login");
+//   }
+//   res.redirect("/urls");
+// });
+
+// app.get("/usernotLogin", (req, res) => {
+//   res.render("usernotLogin");
+// });
 app.get("/urls", (req, res) => {
-  const templateVars = { user: users[req.cookies['user_id']], urls: urlDatabase };
+  const cookiesUser = req.cookies.user_id;
+  const filterUrlByUser = getUserUrls(urlDatabase, cookiesUser);
+  const templateVars = { user: users[cookiesUser], urls: filterUrlByUser };
   res.render("urls_index", templateVars);
+
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: users[req.cookies['user_id']] };
+  if (!templateVars.user) {
+    res.redirect("/login");
+  } else {
   res.render("urls_new", templateVars);
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies['username']] };
+
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: users[req.cookies['user_id']] };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
 
   const shortUrl = generateRandomString()
-  console.log("body longURL: ", req.body.longURL);  // Log the POST request body to the 
-  urlDatabase[shortUrl] = req.body.longURL;
-  res.redirect(`urls/${shortUrl}`);            // Respond with 'Ok' (we will replace this)
+  // console.log("body longURL: ", req.body.longURL);  // Log the POST request body to the 
+  console.log('urlDatabase', urlDatabase);
+  urlDatabase[shortUrl] = {
+    longURL: req.body.longURL,
+    user_id: req.cookies['user_id'],
+
+  };
+  console.log(urlDatabase[shortUrl]);
+  res.redirect(`/urls/${shortUrl}`);            // Respond with 'Ok' 
 });
 
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = req.body.longURL;
-  urlDatabase[id] = longURL;
+    urlDatabase[id] = {
+    longURL: req.body.longURL,
+    user_id: req.cookies['user_id'],
+
+  };
+  //console.log(urlDatabase);
   res.redirect(`/urls`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const cookieUser = req.cookies.user_id;
+  const shortURL = req.params.shortURL;
+  if (cookieUser !== urlDatabase[shortURL]["user_id"]) {
+    return res.status(403).send("permition to delete is denied!");
+   
+  } else {
   delete urlDatabase[req.params.shortURL];
   res.redirect(`/urls`);
+  }
 });
 
+app.post("/urls/:shortURL/update", (req, res) => {
+  const cookieUser = req.cookies.user_id;
+  const shortURL = req.params.shortURL;
+  //check if logged in user matches the userID. If not editing is denied
 
+  if (cookieUser !== urlDatabase[shortURL]["user_id"]) {
+    return res.status(403).send("permition is Denied!");
+  } else {
+    urlDatabase[shortURL] = {
+      longURL: req.body.longURL,
+      user_id: req.cookies['user_id'],  
+    };
+    res.redirect("/urls");
+  }
+});
 
 app.get("/register", (req, res) => {
   // console.log("here i am!")
@@ -100,10 +172,10 @@ app.post("/register", (req, res) => {
   const userPass = req.body.password;
 
   if (!userEmail || !userPass) {
-      return res.status(400).send("Please Enter email and password!");
+    return res.status(400).send("Please Enter email and password!");
   } else if (emailCheck(users, userEmail)) {
     //console.log('user not found')
-    res.sendStatus('User already Exits', 400)
+    res.status(400).send("User Already registed!");
 
   } else {
     const user_id = generateRandomString();  // add new user 
@@ -111,7 +183,7 @@ app.post("/register", (req, res) => {
       id: user_id, email: req.body.email,
       password: req.body.password
     };
-    res.cookie('user_id')
+    res.cookie('user_id', user_id)
     res.redirect("/urls")
   }
 
@@ -131,7 +203,7 @@ app.post("/login", (req, res) => {
   const userPass = req.body.password;
 
   const user = emailCheck(users, userEmail)
-  console.log(users[user].email)
+  // console.log(users[user].email)
 
   if (user) {
     if (userPass === users[user].password) {
@@ -139,12 +211,12 @@ app.post("/login", (req, res) => {
       res.redirect('/urls')
     }
   }
-  res.sendStatus(403)
+  res.status(403).redirect('register')
 });
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id")
-  res.redirect('/urls')
+  res.redirect('/login')
 });
 
 
